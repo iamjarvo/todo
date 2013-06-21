e779f33ccf1a5d482c8ad98a26021d38765294b7
Jearvon Dharrie <Jearvon_Dharrie@cable.comcast.com>
2013-06-20 22:56:21 -0400

    Initial commit

---
+++ config/deploy.rb
@@ -0,0 +1,35 @@
+set :application, "todo"
+set :repository,  "~/code-box/ecofactor/todo/.git"
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm, :git
+
+
+
+role :web, "default"
+role :app, "default"
+set :use_sudo, false
+set :user, "vagrant"
+#set :run_method, :sudo
+set :deploy_to, "~/todo"
+#
+## Must be set for the password prompt from git to work
+# default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+ssh_options[:config] = '.ssh.config'
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = 'c06ae27855ccb52c275d6888ef2405bcba6b924bf830543211371da636d044cbb198492b038f60621d3d0dbd560d911d7447110a1c0bd93d158627d9b742df92'
